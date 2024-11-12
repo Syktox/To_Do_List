0287dfee83a92e8ee577e4e864724bc58b4c51da
syktox <markus.kammerstetter@hotmail.com>
2024-11-12 21:07:07 +0700

    init

---
+++ CMakeFiles/Todo_List.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Todo_List.dir/src/main.cpp.o"
+  "CMakeFiles/Todo_List.dir/src/main.cpp.o.d"
+  "Todo_List"
+  "Todo_List.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Todo_List.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
