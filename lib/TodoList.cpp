18acff948886c29433d50c121f2b5e3967f8f088
syktox <markus.kammerstetter@hotmail.com>
2024-11-13 15:04:26 +0700

    dont know

---
+++ lib/TodoList.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Markus Kammerstetter on 12.11.24.
+//
+
+#include std;
+#include "TodoList.h"
---
+++ lib/TodoList.h
@@ -0,0 +1,19 @@
+//
+// Created by Markus Kammerstetter on 12.11.24.
+//
+
+#ifndef TODO_LIST_TODO_LIST_H
+#define TODO_LIST_TODO_LIST_H
+
+#include "Task.h"
+
+class Todo_List {
+private:
+    std::vector<Task> tasks;
+
+public:
+    Todo_List();
+};
+
+
+#endif //TODO_LIST_TODO_LIST_H
