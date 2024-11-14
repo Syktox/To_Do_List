5ba81330d114a7ab51d6230ca9ccfdd74198ecd7
syktox <markus.kammerstetter@hotmail.com>
2024-11-14 03:04:34 +0000

    added TodoList and functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,22 @@ import Task;
 import TodoList;
 
 int main(int argc, char *argv[]) {
+    TodoList tl;
     
     Task task("Task 1", "Description of Task 1");
-    task.print();
+    Task task2("Task 2", "Description of Task 2");
+    Task task3("Task 3", "Description of Task 3");
+    tl.addTask(task);
+    tl.addTask(task2);
+    tl.addTask(task3);
 
-    TodoList test;
+    tl.printAllTasks();
 
+    tl.deleteTask("Task 1");
+
+    std::cout << "__---___----__-----------" << std::endl;
+    tl.printAllTasks();
+    
+    
     return 0;
 }
